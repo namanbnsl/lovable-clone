0df0dc8abb58d11b12b1a2ee9b60f6b2fcae2ca2
namanb <namanbansal1101@gmail.com>
2025-08-10 17:20:10 +0530

    04-background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-import Client from "@/app/client";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-const Home = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "Naman" }));
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
+const Home = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job invoked successfully!");
+      },
+    })
+  );
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading....</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl">
+      <Button
+        onClick={() => {
+          invoke.mutate({ text: "Naman" });
+        }}
+      >
+        Invoke Background Job
+      </Button>
+    </div>
   );
 };
 
