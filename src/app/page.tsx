76fb19b4f29a8bfd68fac01cacc25ffe5fecb210
namanb <namanbansal1101@gmail.com>
2025-08-10 17:54:43 +0530

    05-ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Home = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -16,9 +19,10 @@ const Home = () => {
   );
   return (
     <div className="p-4 max-w-7xl">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         onClick={() => {
-          invoke.mutate({ text: "Naman" });
+          invoke.mutate({ value: value });
         }}
       >
         Invoke Background Job
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,18 @@
+import { generateText } from "ai"
+import { google } from "@ai-sdk/google"
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    return { message: `Hello ${event.data.email}!` };
+    const {text} = await generateText({
+      model: google("gemini-2.5-pro"),
+      system: "You are an expert summarizer. Summarize the following text in exactly 2 words.",
+      prompt: event.data.value,
+    })
+
+    return { text };
   },
 );
